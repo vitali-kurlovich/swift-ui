e955539f20ac6e5bfc6a1933449c1e9ab7d5a176
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-02-16 19:59:46 +0400

    Fix macOS build

--- Sources/SwiftUIKit/Internal/DynamicTypeSize+UIContentSizeCategory.swift
+++ Sources/SwiftUIKit/Internal/DynamicTypeSize+UIContentSizeCategory.swift
@@ -8,34 +8,38 @@
 import SwiftUI
 
 extension DynamicTypeSize {
-    var contentSizeCategory: UIContentSizeCategory {
-        switch self {
-        case .xSmall:
-            return .extraSmall
-        case .small:
-            return .small
-        case .medium:
-            return .medium
-        case .large:
-            return .large
-        case .xLarge:
-            return .extraLarge
-        case .xxLarge:
-            return .extraExtraLarge
-        case .xxxLarge:
-            return .extraExtraExtraLarge
-        case .accessibility1:
-            return .accessibilityMedium
-        case .accessibility2:
-            return .accessibilityLarge
-        case .accessibility3:
-            return .accessibilityExtraLarge
-        case .accessibility4:
-            return .accessibilityExtraExtraLarge
-        case .accessibility5:
-            return .accessibilityExtraExtraExtraLarge
-        @unknown default:
-            return .medium
+    #if canImport(UIKit)
+
+        var contentSizeCategory: UIContentSizeCategory {
+            switch self {
+            case .xSmall:
+                return .extraSmall
+            case .small:
+                return .small
+            case .medium:
+                return .medium
+            case .large:
+                return .large
+            case .xLarge:
+                return .extraLarge
+            case .xxLarge:
+                return .extraExtraLarge
+            case .xxxLarge:
+                return .extraExtraExtraLarge
+            case .accessibility1:
+                return .accessibilityMedium
+            case .accessibility2:
+                return .accessibilityLarge
+            case .accessibility3:
+                return .accessibilityExtraLarge
+            case .accessibility4:
+                return .accessibilityExtraExtraLarge
+            case .accessibility5:
+                return .accessibilityExtraExtraExtraLarge
+            @unknown default:
+                return .medium
+            }
         }
-    }
+
+    #endif
 }
